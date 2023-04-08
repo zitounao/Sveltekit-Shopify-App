0cb37e70ccca1063dc508ce7a650b7f784d6ea77
Othmane <109449559+zitounao@users.noreply.github.com>
2023-04-08 03:16:48 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,7 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(
-			{
-				out: 'build',
-			}
-		)
+		adapter: adapter(),
 	}
 };
 
